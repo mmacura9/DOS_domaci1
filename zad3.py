701c8e840814ec834fb25b2a3e2347418737ffbe
mmacura9 <44809893+mmacura9@users.noreply.github.com>
2021-11-07 00:45:16 +0100

    Image divided on blocks

--- zad3.py
+++ zad3.py
@@ -9,6 +9,7 @@ import numpy as np
 from skimage import color
 from skimage import io
 import matplotlib.pyplot as plt
+import math
 
 def makeHist2D(imgIn: np.array) -> np.array:
     print("Pravim histogram")
@@ -19,7 +20,7 @@ def makeHist2D(imgIn: np.array) -> np.array:
 
 def makeHist3D(imgIn: np.array) -> np.array:
     numHist = np.zeros([255,3], dtype = int)
-    print(numHist.shape)
+    # print(numHist.shape)
     for i in range(0, 255):
         numHist[i,0]=np.sum((imgIn[:,:,0]>=i) & (imgIn[:,:,0]<i+1))
         numHist[i,1]=np.sum((imgIn[:,:,1]>=i) & (imgIn[:,:,1]<i+1))
@@ -31,19 +32,18 @@ def dosCLAHE(imgIn: np.array, numTiles: [] = [8, 8], limit: float = 0.01) -> np.
     if imgIn.shape[2] == 2:
         print("2D Slika")
         histogram = makeHist2D(imgIn)
-        plt.figure()
-        plt.stem(x, histogram, use_line_collection=True)
     else:
         print("3D Slika")
         # print(makeHist3D(imgIn))
-        histogram = makeHist3D(imgIn)
-        plt.figure()
-        plt.stem(x, histogram[:,0],use_line_collection=True)
-        plt.figure()
-        plt.stem(x, histogram[:,1],use_line_collection=True)
-        plt.figure()
-        plt.stem(x, histogram[:,2],use_line_collection=True)
-
+        sizeX = math.ceil(imgIn.shape[0]/numTiles[0])
+        sizeY = math.ceil(imgIn.shape[1]/numTiles[1])
+        histogram = np.zeros([numTiles[0],numTiles[1],255,3])
+        for i in range(numTiles[0]-1):
+            for j in range(numTiles[1]-1):
+                histogram[i,j,:,:] = makeHist3D(imgIn[i*sizeX:(i+1)*sizeX, j*sizeY:(j+1)*sizeY,:])
+                plt.figure()
+                plt.stem(histogram[i,j,:,0])
+        
 
 imgIn = imread('train.jpg')
 plt.figure()
