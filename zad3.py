75af1665e78dc7001a36f057a0706aeb0c355754
mmacura9 <44809893+mmacura9@users.noreply.github.com>
2021-11-07 21:33:09 +0100

    Issues solved, this task finished

--- zad3.py
+++ zad3.py
@@ -12,14 +12,14 @@ import matplotlib.pyplot as plt
 import math
 
 def makeHist2D(imgIn: np.array) -> np.array:
-    numHist = np.zeros(255, dtype = int)
-    for i in range(0, 255):
+    numHist = np.zeros(256, dtype = int)
+    for i in range(0, 256):
         numHist[i] = np.sum(imgIn == i)
     return numHist/imgIn.size
 
 def makeHist3D(imgIn: np.array) -> np.array:
-    numHist = np.zeros([255,3], dtype = int)
-    for i in range(0, 255):
+    numHist = np.zeros([256,3], dtype = int)
+    for i in range(0, 256):
         numHist[i,0]=np.sum(imgIn[:,:,0]==i)
         numHist[i,1]=np.sum(imgIn[:,:,1]==i)
         numHist[i,2]=np.sum(imgIn[:,:,2]==i)
@@ -94,20 +94,20 @@ def bilinearInterpolation(imgIn: np.array, T: np.array, sizeX: int, sizeY: int, 
             if blockX+pomX<0 or (blockX+pomX)>=numTiles[0]:
                 a = 0
             else:
-                x10 = T[blockX+pomX,blockY,imgIn[i,j,0]-1,0]
-                x11 = T[blockX+pomX,blockY,imgIn[i,j,1]-1,1]
-                x12 = T[blockX+pomX,blockY,imgIn[i,j,2]-1,2]
+                x10 = T[blockX+pomX,blockY,imgIn[i,j,0],0]
+                x11 = T[blockX+pomX,blockY,imgIn[i,j,1],1]
+                x12 = T[blockX+pomX,blockY,imgIn[i,j,2],2]
                 x1 = np.array([x10, x11, x12])
             
             if blockY+pomY>=0 and (blockY+pomY)<numTiles[1]:
-                x20 = T[blockX,blockY+pomY,imgIn[i,j,0]-1,0]
-                x21 = T[blockX,blockY+pomY,imgIn[i,j,1]-1,1]
-                x22 = T[blockX,blockY+pomY,imgIn[i,j,2]-1,2]
+                x20 = T[blockX,blockY+pomY,imgIn[i,j,0],0]
+                x21 = T[blockX,blockY+pomY,imgIn[i,j,1],1]
+                x22 = T[blockX,blockY+pomY,imgIn[i,j,2],2]
                 x2 = np.array([x20, x21, x22])
                 if blockX+pomX>=0 and (blockX+pomX)<numTiles[0]:
-                    x30 = T[blockX+pomX,blockY+pomY,imgIn[i,j,0]-1,0]
-                    x31 = T[blockX+pomX,blockY+pomY,imgIn[i,j,1]-1,1]
-                    x32 = T[blockX+pomX,blockY+pomY,imgIn[i,j,2]-1,2]
+                    x30 = T[blockX+pomX,blockY+pomY,imgIn[i,j,0],0]
+                    x31 = T[blockX+pomX,blockY+pomY,imgIn[i,j,1],1]
+                    x32 = T[blockX+pomX,blockY+pomY,imgIn[i,j,2],2]
                     x3 = np.array([x30, x31, x32])
             
             c = abs(j-centerY)
@@ -116,11 +116,11 @@ def bilinearInterpolation(imgIn: np.array, T: np.array, sizeX: int, sizeY: int, 
             if blockY+pomY<0 or (blockY+pomY)>=numTiles[1]:
                 c = 0
             
-            sh10 = (T[blockX,blockY,imgIn[i,j,0]-1,0]*b + x1[0]*a)/(a+b)
+            sh10 = (T[blockX,blockY,imgIn[i,j,0],0]*b + x1[0]*a)/(a+b)
             
-            sh11 = (T[blockX,blockY,imgIn[i,j,1]-1,1]*b + x1[1]*a)/(a+b)
+            sh11 = (T[blockX,blockY,imgIn[i,j,1],1]*b + x1[1]*a)/(a+b)
             
-            sh12 = (T[blockX,blockY,imgIn[i,j,2]-1,2]*b + x1[2]*a)/(a+b)
+            sh12 = (T[blockX,blockY,imgIn[i,j,2],2]*b + x1[2]*a)/(a+b)
             
             sh1 = np.array([sh10, sh11, sh12])
             sh2 = (x2*b+x3*a)/(a+b)
@@ -134,7 +134,7 @@ def dosCLAHE(imgIn: np.array, numTiles: [] = [8, 8], limit: float = 0.01) -> np.
     sizeY = math.ceil(imgIn.shape[1]/numTiles[1])
     if imgIn.shape[2] == 3:
         print("3D Slika")
-        T = np.zeros([numTiles[0],numTiles[1],255,3])
+        T = np.zeros([numTiles[0],numTiles[1],256,3])
         for i in range(numTiles[0]):
             for j in range(numTiles[1]):
                 mat = makeMat3D(imgIn, sizeX, sizeY, i, j)
