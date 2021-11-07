311afa8879ed16e3c2daff24800d35e649c15b25
mmacura9 <44809893+mmacura9@users.noreply.github.com>
2021-11-07 01:14:58 +0100

    Block Divition Almost Done

--- zad3.py
+++ zad3.py
@@ -27,6 +27,23 @@ def makeHist3D(imgIn: np.array) -> np.array:
         numHist[i,2]=np.sum((imgIn[:,:,2]>=i) & (imgIn[:,:,2]<i+1))
     return numHist/np.size(imgIn[:,:,0])
 
+def makeMat(imgIn: np.array, sizeX: int, sizeY: int, i: int, j: int) -> np.array:
+    if (i+1)*sizeX <= imgIn.shape[0] and (j+1)*sizeY <= imgIn.shape[1]:
+        return imgIn[i*sizeX:(i+1)*sizeX, j*sizeY:(j+1)*sizeY, :]
+    if (i+1)*sizeX > imgIn.shape[0] and (j+1)*sizeY <= imgIn.shape[1]:
+        mat = imgIn[i*sizeX:, j*sizeY:(j+1)*sizeY, :]
+        con = imgIn[imgIn.shape[0]-(i+1)*sizeX:, j*sizeY:(j+1)*sizeY, :]
+        return np.concatenate((mat, con), axis = 0)
+    if (i+1)*sizeX <= imgIn.shape[0] and (j+1)*sizeY > imgIn.shape[1]:
+        mat = imgIn[i*sizeX:(i+1)*sizeX, j*sizeY:, :]
+        con = imgIn[i*sizeX:(i+1)*sizeX, imgIn.shape[1]-(j+1)*sizeY:, :]
+        return np.concatenate((mat, con), axis = 1)
+    mat = imgIn[i*sizeX:, j*sizeY:, :]
+    con1 = imgIn[i*sizeX:, imgIn.shape[1]-(j+1)*sizeY:, :]
+    mat = np.concatenate((mat, con1), axis = 1)
+    con2 = imgIn[imgIn.shape[0]-(i+1)*sizeX:, j*sizeY:, :]
+    return np.concatenate((mat, con2), axis = 0)
+
 def dosCLAHE(imgIn: np.array, numTiles: [] = [8, 8], limit: float = 0.01) -> np.array:
     x = np.linspace(0,255,255)
     if imgIn.shape[2] == 2:
@@ -38,11 +55,10 @@ def dosCLAHE(imgIn: np.array, numTiles: [] = [8, 8], limit: float = 0.01) -> np.
         sizeX = math.ceil(imgIn.shape[0]/numTiles[0])
         sizeY = math.ceil(imgIn.shape[1]/numTiles[1])
         histogram = np.zeros([numTiles[0],numTiles[1],255,3])
-        for i in range(numTiles[0]-1):
-            for j in range(numTiles[1]-1):
-                histogram[i,j,:,:] = makeHist3D(imgIn[i*sizeX:(i+1)*sizeX, j*sizeY:(j+1)*sizeY,:])
-                plt.figure()
-                plt.stem(histogram[i,j,:,0])
+        for i in range(numTiles[0]):
+            for j in range(numTiles[1]):
+                mat = makeMat(imgIn, sizeX, sizeY, i, j)
+                histogram[i,j,:,:] = makeHist3D(mat)
         
 
 imgIn = imread('train.jpg')
