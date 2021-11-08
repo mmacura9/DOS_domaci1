18bcbd809d91a246028559de78e5baf21675bcea
mmacura9 <44809893+mmacura9@users.noreply.github.com>
2021-11-08 21:15:26 +0100

    Queen dress issue solved

--- zad1.py
+++ zad1.py
@@ -21,49 +21,11 @@ import ipywidgets as widgets
 
 import numpy as np
 
-def dressQueenLCH(rgbQueen: np.array, rgbImg: np.array) -> np.array:
-    imgQueenLab = color.rgb2lab(rgbQueen)
-    imgQueenLCh = color.lab2lch(imgQueenLab)
-    
-    Labimg = color.rgb2lab(rgbImg)
-    LChimg = color.lab2lch(Labimg)
-    
-    fig, ax = plt.subplots(2, 2, figsize=(12,9), dpi=80);
-    tight_layout();
-    ax[0,0].imshow(rgbQueen); ax[0,0].set_title('RGB', fontsize=14);
-    ax[0,1].imshow(imgQueenLCh[:,:,0], vmin=np.min(imgQueenLCh[:,:,0]), vmax=np.max(imgQueenLCh[:,:,0]), cmap='gray'); ax[0,1].set_title('L', fontsize=14);
-    ax[1,0].imshow(imgQueenLCh[:,:,1], vmin=np.min(imgQueenLCh[:,:,1]), vmax=np.max(imgQueenLCh[:,:,1]), cmap='jet'); ax[1,0].set_title('C', fontsize=14);
-    ax[1,1].imshow(imgQueenLCh[:,:,2], vmin=0, vmax=2*pi, cmap='jet'); ax[1,1].set_title('h', fontsize=14);
-    
-    downTreshold = 3*pi/4
-    upTreshold = 5*pi/4
-    mask = (imgQueenLCh[:,:,2]>downTreshold) & (imgQueenLCh[:,:,2]<upTreshold)
-    mask1 = mask
-    
-    mask = mask*1.
-    mask_gauss = filters.gaussian(mask, sigma=10, truncate=5)
-    
-    newH = np.ones(rgbQueen[:,:,0].shape)*LChimg[0,0,2]
-    newH[416:832,200:616] = LChimg[:,:,2]
-    
-    newC = np.ones(rgbQueen[:,:,0].shape)*LChimg[0,0,1]
-    newC[416:832,200:616] = LChimg[:,:,1]
-    
-    mask1 = mask_gauss>0.5
-    
-    imgQueenLCh[:,:,2] = mask1*newH + ~mask1*imgQueenLCh[:,:,2]
-    imgQueenLCh[:,:,1] = mask1*newC + ~mask1*imgQueenLCh[:,:,1]
-    
-    imgQueenLab = color.lch2lab(imgQueenLCh)
-    imgQueenOutput = color.lab2rgb(imgQueenLab)
-    fig = plt.figure()
-    io.imshow(imgQueenOutput)
-    
-    return imgQueenOutput
-
 def dressQueenHSVGauss(rgbQueen: np.array, rgbImg: np.array) -> np.array:
     imgQueenHSV = color.rgb2hsv(rgbQueen)
-    imgHSV = color.rgb2hsv(rgbImg)
+    # imgHSV = color.rgb2hsv(rgbImg)
+    imgYUV = color.rgb2yuv(rgbImg)
+    imgQueenYUV = color.rgb2yuv(rgbQueen)
     
     fig, ax = plt.subplots(2, 2, figsize=(12,9), dpi=80);
     tight_layout();
@@ -72,72 +34,32 @@ def dressQueenHSVGauss(rgbQueen: np.array, rgbImg: np.array) -> np.array:
     ax[1,0].imshow(imgQueenHSV[:,:,1], vmin=0, vmax=1, cmap='jet'); ax[1,0].set_title('S', fontsize=14);
     ax[1,1].imshow(imgQueenHSV[:,:,0], vmin=0, vmax=1, cmap='jet'); ax[1,1].set_title('H', fontsize=14);
     
-    mask = (imgQueenHSV[:,:,0]>0.4) & (imgQueenHSV[:,:,0]<0.6)
+    mask = (imgQueenHSV[:,:,0]>0.4) & (imgQueenHSV[:,:,0]<0.6) & (imgQueenHSV[:,:,1]>0.5)
     mask1 = mask
     
     mask = mask*1.
-    mask_gauss = filters.gaussian(mask, sigma=9, truncate=5)
+    mask_gauss = filters.gaussian(mask, sigma=1, truncate=5)
     
-    newH = np.ones(rgbQueen[:,:,0].shape)*imgHSV[0,0,0]
-    newH[416:832,200:616] = imgHSV[:,:,0]
+    newU = np.ones(rgbQueen[:,:,0].shape)*imgYUV[0,0,1]
+    newU[416:832,200:616] = imgYUV[:,:,1]
     
-    newS = np.ones(rgbQueen[:,:,0].shape)*imgHSV[0,0,1]
-    newS[416:832,200:616] = imgHSV[:,:,1]
+    newV = np.ones(rgbQueen[:,:,0].shape)*imgYUV[0,0,2]
+    newV[416:832,200:616] = imgYUV[:,:,2]
     
-    # newH[newH<0.5] = -1
-    # newV = np.ones(rgbQueen[:,:,0].shape)*imgHSV[0,0,2]
-    # newV[416:832,200:616] = imgHSV[:,:,2]
+    mask1 = mask_gauss>0.1
     
-    mask1 = mask_gauss>0.5
+    outputU = mask1*newU
+    outputV = mask1*newV
     
-    outputH = mask1*newH
-    outputS = mask1*newS
-    # outputV = mask_gauss*newV
+    imgQueenYUV[mask1,1] = outputU[mask1]
+    imgQueenYUV[mask1,2] = outputV[mask1]
     
-    imgQueenHSV[mask1,0] = outputH[mask1]
-    imgQueenHSV[mask1,1] = outputS[mask1]
-    # imgQueenHSV[mask1,2] = outputV[mask1]
-    
-    imgQueenOutput = color.hsv2rgb(imgQueenHSV)
+    imgQueenOutput = color.yuv2rgb(imgQueenYUV)
     
     plt.figure()
     io.imshow(imgQueenOutput)
     return imgQueenOutput
 
-def dressQueenHSVBox(rgbQueen: np.array, rgbImg: np.array) -> np.array:
-    imgQueenHSV = color.rgb2hsv(rgbQueen)
-    imgHSV = color.rgb2hsv(rgbImg)
-    
-    fig, ax = plt.subplots(2, 2, figsize=(12,9), dpi=80);
-    tight_layout();
-    ax[0,0].imshow(rgbQueen); ax[0,0].set_title('RGB', fontsize=14);
-    ax[0,1].imshow(imgQueenHSV[:,:,2], vmin=0, vmax=1, cmap='gray'); ax[0,1].set_title('V', fontsize=14);
-    ax[1,0].imshow(imgQueenHSV[:,:,1], vmin=0, vmax=1, cmap='jet'); ax[1,0].set_title('S', fontsize=14);
-    ax[1,1].imshow(imgQueenHSV[:,:,0], vmin=0, vmax=1, cmap='jet'); ax[1,1].set_title('H', fontsize=14);
-    
-    mask = (imgQueenHSV[:,:,0]>0.4) & (imgQueenHSV[:,:,0]<0.6)
-    mask1 = mask
-    
-    mask = mask*1.
-    box_mask = np.ones([30,30], dtype=float)/900
-    box_mask = ndimage.correlate(mask, box_mask)
-    
-    newH = np.ones(rgbQueen[:,:,0].shape)*imgHSV[0,0,0]
-    newH[416:832,200:616] = imgHSV[:,:,0]
-    
-    newS = np.ones(rgbQueen[:,:,0].shape)*imgHSV[0,0,1]
-    newS[416:832,200:616] = imgHSV[:,:,1]
-    
-    mask1 = box_mask>0.5
-    
-    imgQueenHSV[:,:,0] = mask1*newH + ~mask1*imgQueenHSV[:,:,0]
-    imgQueenHSV[:,:,1] = mask1*newS + ~mask1*imgQueenHSV[:,:,1]
-    
-    imgQueenOutput = color.hsv2rgb(imgQueenHSV)
-    
-    plt.figure()
-    io.imshow(imgQueenOutput)
-    return imgQueenOutput
 
 if __name__=="__main__":
     imgQueen = imread('queen_dress.jpg')
