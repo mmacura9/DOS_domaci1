828357981c913b4ad3aa51f9483b9f38b69ae228
mmacura9 <44809893+mmacura9@users.noreply.github.com>
2021-11-11 00:49:40 +0100

    Video editing done

--- zad1.py
+++ zad1.py
@@ -23,10 +23,9 @@ import ipywidgets as widgets
 
 import numpy as np
 
-def dressQueenHSVGauss(rgbQueen: np.array, rgbImg: np.array) -> np.array:
+def dressQueenHSVGauss(rgbQueen: np.array, yuvImg: np.array) -> np.array:
     imgQueenHSV = color.rgb2hsv(rgbQueen)
     # imgHSV = color.rgb2hsv(rgbImg)
-    imgYUV = color.rgb2yuv(rgbImg)
     imgQueenYUV = color.rgb2yuv(rgbQueen)
     yComp = imgQueenYUV[:,:,0]/np.max(imgQueenYUV[:,:,0])
     size1 = rgbQueen.shape
@@ -38,37 +37,23 @@ def dressQueenHSVGauss(rgbQueen: np.array, rgbImg: np.array) -> np.array:
     # ax[1,0].imshow(imgQueenHSV[:,:,1], vmin=0, vmax=1, cmap='jet'); ax[1,0].set_title('S', fontsize=14);
     # ax[1,1].imshow(imgQueenHSV[:,:,0], vmin=0, vmax=1, cmap='jet'); ax[1,1].set_title('H', fontsize=14);
     
-    # fig, ax = plt.subplots(2, 2, figsize=(12,9), dpi=80);
-    # tight_layout();
-    # ax[0,0].imshow(rgbQueen); ax[0,0].set_title('RGB', fontsize=14);
-    # ax[0,1].imshow(imgQueenYUV[:,:,2], vmin=0, vmax=1, cmap='gray'); ax[0,1].set_title('Y', fontsize=14);
-    # ax[1,0].imshow(imgQueenYUV[:,:,1], vmin=-0.5, vmax=0.5, cmap='jet'); ax[1,0].set_title('U', fontsize=14);
-    # ax[1,1].imshow(imgQueenYUV[:,:,0], vmin=-0.5, vmax=0.5, cmap='jet'); ax[1,1].set_title('V', fontsize=14);
+    mask = (imgQueenHSV[:,:,0]>0.2) & (imgQueenHSV[:,:,0]<0.5) & (imgQueenHSV[:,:,1]>0.4)
     
-    # fig, ax = plt.subplots(2, 2, figsize=(12,9), dpi=80);
-    # tight_layout();
-    # ax[0,0].imshow(rgbImg); ax[0,0].set_title('RGB', fontsize=14);
-    # ax[0,1].imshow(imgYUV[:,:,2], vmin=0, vmax=1, cmap='gray'); ax[0,1].set_title('Y', fontsize=14);
-    # ax[1,0].imshow(imgYUV[:,:,1], vmin=-0.5, vmax=0.5, cmap='jet'); ax[1,0].set_title('U', fontsize=14);
-    # ax[1,1].imshow(imgYUV[:,:,0], vmin=-0.5, vmax=0.5, cmap='jet'); ax[1,1].set_title('V', fontsize=14);
+    mask = mask*imgQueenHSV[:,:,1]
+    mask_gauss = filters.gaussian(mask, sigma=10, truncate=0.1)
     
-    mask = (imgQueenHSV[:,:,0]>0.2) & (imgQueenHSV[:,:,0]<0.5) & (imgQueenHSV[:,:,1]>0.75)
-    mask1 = mask
-    
-    mask = mask*1.
-    mask_gauss = filters.gaussian(mask, sigma=3, truncate=4)
-    
-    # newY = np.ones(rgbQueen[:,:,0].shape)*imgYUV[0,0,0]
     newY = imgYUV[0:size1[0],0:size1[1],0]
     
-    # newU = np.ones(rgbQueen[:,:,0].shape)*imgYUV[0,0,1]
     newU = imgYUV[0:size1[0],0:size1[1],1]
     
-    # newV = np.ones(rgbQueen[:,:,0].shape)*imgYUV[0,0,2]
     newV = imgYUV[0:size1[0],0:size1[1],2]
     
-    mask1 = mask_gauss>0.2
+    mask1 = mask_gauss>0.4
     
+    if np.sum(mask1)<6000:
+        return rgbQueen
+    
+    # print(np.sum(mask1))
     outputU = mask1*newU
     outputV = mask1*newV
     
@@ -78,23 +63,19 @@ def dressQueenHSVGauss(rgbQueen: np.array, rgbImg: np.array) -> np.array:
     
     imgQueenOutput = color.yuv2rgb(imgQueenYUV)
     
-    # plt.figure()
-    # io.imshow(imgQueenOutput)
     return imgQueenOutput
 
 
 if __name__=="__main__":
     imgQueen = imread('queen_dress.jpg')
-    imgRacism = imread('slika.jpg')
-    
+    rgbImg = imread('slika.jpg')
+    imgYUV = color.rgb2yuv(rgbImg)
     filename = 'queen_coat.mp4'
     
     vid = imageio.get_reader(filename,  'ffmpeg')
     video_out = imageio.get_writer('queen_coat_out.mp4', format='FFMPEG', mode = 'I', fps = 30, codec = 'h264')
-    i=0
     for cur_frame in vid:
-        i=i+1
-        cur_frame = dressQueenHSVGauss(cur_frame, imgRacism)
+        cur_frame = dressQueenHSVGauss(cur_frame, imgYUV)
         video_out.append_data(cur_frame)
     
     video_out.close()
